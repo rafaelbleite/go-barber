a6a8fddabd38e882bd75077eb2dd7d482e9aa1d8
Rafael Leite <rafaelleite.sa@gmail.com>
2020-07-05 21:08:14 -0300

    Backend Fix List Providers

--- backend/src/modules/appointments/services/ListProvidersService.ts
+++ backend/src/modules/appointments/services/ListProvidersService.ts
@@ -1,4 +1,5 @@
 import { injectable, inject } from 'tsyringe';
+import { classToClass } from 'class-transformer';
 
 import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
@@ -31,7 +32,7 @@ class ListProvidersService {
 
       await this.cacheProvider.save(
         `providers-list:${user_id}`,
-        JSON.stringify(users),
+        classToClass(users),
       );
     }
 
