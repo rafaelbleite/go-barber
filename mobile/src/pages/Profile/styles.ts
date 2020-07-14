c3a37bb28a3e0cc5364819975ce79c7c4b7e3e74
Rafael Leite <rafaelleite.sa@gmail.com>
2020-07-14 00:23:24 -0300

    Mobile - Finalizando app

---
+++ mobile/src/pages/Profile/styles.ts
@@ -0,0 +1,32 @@
+import styled from 'styled-components/native';
+import { Platform } from 'react-native';
+
+export const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  /* padding: 0 30px ${Platform.OS === 'android' ? 50 : 40}px; */
+  padding: 10px 30px 20px;
+`;
+
+export const BackButton = styled.TouchableOpacity`
+  margin-top: 40px;
+`;
+
+export const UserAvatarButton = styled.TouchableOpacity`
+  margin-top: 32px;
+`;
+
+export const UserAvatar = styled.Image`
+  width: 150px;
+  height: 150px;
+  border-radius: 75px;
+  align-self: center;
+`;
+
+export const Title = styled.Text`
+  font-size: 20px;
+  color: #f4ede8;
+  font-family: 'RobotoSlab-Medium';
+  margin: 24px 0 24px;
+  text-align: left;
+`;
